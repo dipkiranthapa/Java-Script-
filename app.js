d1269969a8d3c2c6734db2ce9b809d1370c6755e
dipkiran <dipkiran2008@gmail.com>
2026-03-25 15:15:52 +0400

    added loop

--- app.js
+++ app.js
@@ -76,3 +76,85 @@ console.log("------------------Operator--------------");
 // console.log(5 * 6);
 // console.log(15 / 6);
 // console.log(15 % 6);
+
+// Relational Operator
+// console.log(10 < 15) Less then True
+// console.log(10 > 15) 10 is greater then 15. False
+
+// if- else statement
+// let light = "green";
+// if (light === "red") {
+//   console.log("stop");
+// } else if (light === "green") {
+//   console.log("go");
+// }
+
+// Switch
+
+// const day = "Tusday";
+// switch (day) {
+//   case "Sunday":
+//     console.log("Holiday");
+//     break;
+//   case "Monday":
+//     console.log("Working day");
+//     break;
+//   case "Tusday":
+//     console.log("Working Day");
+//     break;
+// }
+
+// Ternary Operator
+// let age = 0;
+// age >= 18 ? console.log("Adult") : console.log("Children");
+
+console.log("---------------Loop-----------------");
+// Loop Repeating over and over, round and round
+// 1. for
+// 2. while
+// 3. do while
+// 4 .forEach
+
+// // For
+// for(start PointerEvent; CSSConditionRule; increment/decrement)
+// increment value will be at start
+// let a = 1;
+// for (let a = 1; a <= 100; a++) {
+//   console.log(a);
+// }
+
+//While loop
+// Increment value will be at end
+// while(condition){
+//   code
+// }
+
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+
+console.log("------------------list----------");
+// const student = ["ram", "hari", "shyam"];
+// for (let i = 0; i < 3; i++) {
+//   console.log(student[i]);
+// }
+
+// For Each
+// const student = ["ram", "hari", "shyam"];
+// student.forEach(function (name) {
+//   console.log(name);
+// });
+
+// Using for...of (modern & simple)
+// const student = ["ram", "hari", "shyam"];
+// for (let name of student) {
+//   console.log(name);
+// }
+
+// Using a loop (most common)
+// const student = ["ram", "hari", "shyam"];
+// for (let i = 0; i < student.length; i++) {
+//   console.log(student[i]);
+// }
